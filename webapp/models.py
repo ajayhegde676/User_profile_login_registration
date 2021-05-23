c60550b57bc9672de0ba05f2928da2b720794636
ajayhegde676 <81635576+ajayhegde676@users.noreply.github.com>
2021-05-23 13:43:42 +0530

    Update models.py

--- webapp/models.py
+++ webapp/models.py
@@ -8,4 +8,4 @@ class User_data(models.Model):
     email = models.EmailField(max_length=254,unique=True)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
+    
